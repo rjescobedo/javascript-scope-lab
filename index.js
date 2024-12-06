a12aa6580b970c17e0a844fb8ba1db9f8a7cdc95
Raul Escobedo <rjescobedo@gmail.com>
2024-12-05 18:18:36 -0600

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+// Step 1: Declared global variables
+const burgers = ['Hamburger', 'Cheeseburger'];
+let featuredDrink = 'Strawberry Milkshake';
+
+// Step 2: Function and Block
+function addBurger() {
+    const newBurger = 'Flatburger';
+    burgers.push(newBurger);
+
+    // Step 3: Variable Decleration and Scope
+    if(true) {
+        const anotherNewBurger = 'Maple Bacon Burger';
+        burgers.push(anotherNewBurger);
+    }
+}
+
+function changeFeaturedDrink() {
+    featuredDrink = 'The JavaShake';
+}
+
+
+
